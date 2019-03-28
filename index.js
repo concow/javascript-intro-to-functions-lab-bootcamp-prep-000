fe6fbee78b0f5dcdbf0057e2eff8ccca00539db4
Connor Cowan <ccowan003@comcast.net>
2019-03-28 00:14:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,20 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  return console.log.toUpperCase()
+  console.log(string.toUpperCase())
 }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I cant't hear you!"
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
